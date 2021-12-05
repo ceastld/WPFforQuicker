34d053bb067affd349acbb58ffd6f0adff8fbd95
ceastld <61349137+dyliu02@users.noreply.github.com>
2021-12-05 20:31:53 +0800

    开始demo

---
+++ WPFforQuicker/Tutorial/GetStart/CSStepDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFforQuicker.Tutorial
+{
+    using Quicker.Public;
+    using System.Windows;
+    internal class CSStepDemo
+    {
+        public static void Exec(IStepContext context)
+        {
+            string text = (string)context.GetVarValue("text");
+            MessageBox.Show("动作中text变量当前值:" + text);
+            context.SetVarValue("text", "hhhh");
+            MessageBox.Show("设置动作中text变量等待值后:" + context.GetVarValue("text"));
+        }
+    }
+}
---
+++ WPFforQuicker/Tutorial/GetStart/CSStepDemoForWPF.cs
@@ -0,0 +1,35 @@
+﻿using Quicker.Public;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFforQuicker.Tutorial.GetStart
+{
+    public class CSStepDemoForWPF
+    {
+        public static void Exec(IStepContext context)
+        {
+            //一个窗口
+            var window = new Window()
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+            };
+            //设置文字内容
+            window.Content = new TextBlock()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Text = "Hello World!",
+                FontSize = 50
+            };
+            //显示窗口
+            window.Show();
+        }
+        public static void UIThreadDemo()
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                //在这里写上你的代码
+            });
+        }
+    }
+}
